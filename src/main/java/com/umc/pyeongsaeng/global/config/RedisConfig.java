8d08360fbd7c9190d3213e0f916e948bedeb8799
leewatertrue <leewatertrue@naver.com>
2025-07-02 15:21:19 +0900

    feat: Redis 및 JWT 기본 설정 추가

---
+++ src/main/java/com/umc/pyeongsaeng/global/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.umc.pyeongsaeng.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host:localhost}")
+	private String host;
+
+	@Value("${spring.data.redis.port:6379}")
+	private int port;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(host, port);
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory());
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+		return template;
+	}
+}
