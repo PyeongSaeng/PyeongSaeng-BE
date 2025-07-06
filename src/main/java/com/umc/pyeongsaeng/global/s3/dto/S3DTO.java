3493bae1cb393211ae36d6727e7f66edf1409537
박지현 <wlgusqkr11@naver.com>
2025-07-06 21:54:27 +0900

    feat:S3Configê°oad, uploapresigned-url d를 위한 dto, 추가

--- src/main/java/com/umc/pyeongsaeng/global/s3/dto/S3DTO.java
+++ src/main/java/com/umc/pyeongsaeng/global/s3/dto/S3DTO.java
@@ -1,2 +1,48 @@
-package com.umc.pyeongsaeng.global.s3.dto;public class S3DTO {
+package com.umc.pyeongsaeng.global.s3.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class S3DTO {
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class PresignedUrlToUploadResponse {
+		private String keyName;
+		private String url;
+	}
+
+
+	@Getter
+	public static class PresignedUrlToUploadRequest {
+
+		@NotBlank
+		@NotNull
+		@Length(min = 1, max = 30)
+		private String fileName;
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class PresignedUrlToDownloadResponse {
+		private String url;
+	}
+
+	@Getter
+	public static class PresignedUrlToDownloadRequest {
+
+		@NotBlank
+		@NotNull
+		private String keyName;
+	}
 }
