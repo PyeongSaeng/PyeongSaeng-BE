3451b97560b687e13eeaaf1b93d7b3252757cf1d
박지현 <wlgusqkr11@naver.com>
2025-07-14 20:26:30 +0900

    fix:healthCheck컨트롤러 이름 변경, Security Public_ENDPOINTS에 health 추가

--- src/main/java/com/umc/pyeongsaeng/global/common/HealthController.java
+++ src/main/java/com/umc/pyeongsaeng/global/common/HealthController.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/actuator")
 public class HealthController {
 
 	@Getter
