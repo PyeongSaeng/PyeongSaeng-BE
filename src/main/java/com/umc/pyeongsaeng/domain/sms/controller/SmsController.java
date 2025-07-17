1fe3f4301fbb93d5e37ec6824852b818ab44506f
songhee <songhee9327@gmail.com>
2025-07-17 15:47:13 +0900

    chore: Swagger 보안 설정 및 인증 비요구 API 처리

--- src/main/java/com/umc/pyeongsaeng/domain/sms/controller/SmsController.java
+++ src/main/java/com/umc/pyeongsaeng/domain/sms/controller/SmsController.java
@@ -13,6 +13,7 @@ import com.umc.pyeongsaeng.global.apiPayload.code.status.SuccessStatus;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +26,7 @@ public class SmsController {
 	private final SmsService smsService;
 
 	@PostMapping("/send")
+	@SecurityRequirements
 	@Operation(summary = "SMS 인증번호 발송", description = "입력된 전화번호로 인증번호를 발송합니다.")
 	@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "SMS201", description = "인증번호가 발송되었습니다.")
 	public ApiResponse<String> sendSmsVerification(
@@ -35,6 +37,7 @@ public class SmsController {
 	}
 
 	@PostMapping("/verify")
+	@SecurityRequirements
 	@Operation(summary = "SMS 인증번호 확인",
 		description = "SMS 인증번호를 확인합니다. 올바른 인증번호인 경우 성공 응답을 반환합니다.")
 	@ApiResponses({
--- src/main/java/com/umc/pyeongsaeng/global/config/SwaggerConfig.java
+++ src/main/java/com/umc/pyeongsaeng/global/config/SwaggerConfig.java
@@ -6,14 +6,28 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 	@Bean
 	public OpenAPI openAPI() {
+
+		String jwtSchemeName = "JWT Authentication";
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+		Components components = new Components()
+			.addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+				.name(jwtSchemeName)
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("JWT"));
+
+
 		return new OpenAPI()
-			.components(new Components())
-			.info(apiInfo());
+			.info(apiInfo())
+			.addSecurityItem(securityRequirement)
+			.components(components);
 	}
 
 	private Info apiInfo() {
