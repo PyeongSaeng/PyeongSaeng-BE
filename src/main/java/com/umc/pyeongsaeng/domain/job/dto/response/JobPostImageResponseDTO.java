544d42fac0b397024aa6de3fb886ab7afbd252e8
박지현 <wlgusqkr11@naver.com>
2025-07-20 10:59:40 +0900

    feat : JobPostImage DTO, Converter 추가

---
+++ src/main/java/com/umc/pyeongsaeng/domain/job/dto/response/JobPostImageResponseDTO.java
@@ -0,0 +1,20 @@
+package com.umc.pyeongsaeng.domain.job.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class JobPostImageResponseDTO {
+
+	@Builder
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class JobPostImagePreviewDTO {
+
+		Long jobPostId;
+
+		String keyName;
+	}
+}
