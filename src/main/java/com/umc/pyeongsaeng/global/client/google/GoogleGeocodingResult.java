de00db1cbe76f99c1180b5e0e328aaa77fbd86bc
songhee <songhee9327@gmail.com>
2025-07-26 13:33:35 +0900

    refactor: GoogleGeocodingResult GeoPoint 제거

--- src/main/java/com/umc/pyeongsaeng/global/client/google/GoogleGeocodingResult.java
+++ src/main/java/com/umc/pyeongsaeng/global/client/google/GoogleGeocodingResult.java
@@ -1,12 +1,11 @@
 package com.umc.pyeongsaeng.global.client.google;
 
-import org.springframework.data.elasticsearch.core.geo.GeoPoint;
-
 public record GoogleGeocodingResult(
 	String sido,
 	String sigungu,
 	String bname,
-	GeoPoint geoPoint
+	Double lat,
+	Double lon
 ) {}
 
 
