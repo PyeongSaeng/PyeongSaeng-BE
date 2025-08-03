9950fa0905b7f5a5871209a454e169eaa7ca345e
zzaengee <odds2698@naver.com>
2025-08-03 17:56:43 +0900

    [feat] ai 프롬프트 수정

--- src/main/java/com/umc/pyeongsaeng/domain/application/util/OpenAiPromptBuilder.java
+++ src/main/java/com/umc/pyeongsaeng/domain/application/util/OpenAiPromptBuilder.java
@@ -13,7 +13,7 @@ public class OpenAiPromptBuilder {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append(buildUserProfile(request.getAnswers()));
-		sb.append("위의 정보를 바탕으로 아래 질문에 대한 답변의 핵심 문장 키워드를 3개 생성해줘.\n");
+		sb.append("위의 정보를 바탕으로 아래 질문에 대한 답변의 핵심 문장 키워드를 3개 생성해줘. 리스트 번호 없이 텍스트만 생성해주면 돼.\n");
 		sb.append("지원자는 시니어이며, 질문은 다음과 같아.\n");
 		sb.append("질문: ").append(request.getQuestion());
 
@@ -26,7 +26,7 @@ public class OpenAiPromptBuilder {
 
 		sb.append(buildUserProfile(request.getAnswers()));
 		sb.append("선택된 키워드: ").append(request.getSelectedKeyword()).append("\n\n");
-		sb.append("위 정보를 바탕으로 아래 질문에 대한 답변을 200자 이내로 작성해줘.\n");
+		sb.append("위 정보를 바탕으로 아래 질문에 대한 답변을 200자 이내로 작성해줘. 노인이 작성한다고 생각하고 알맞게 현실적으로 작성해줘.\n");
 		sb.append("질문: ").append(request.getQuestion());
 
 		return sb.toString();
