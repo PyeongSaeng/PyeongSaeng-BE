e267e88fa7eb108d5d56f75457635d409e0723bc
parkjihyeon <wlgusqkr11@naver.com>
2025-08-05 02:03:39 +0900

    fix: 머지하다가 생긴 오류 수정 JobPostFormField제거

--- src/main/java/com/umc/pyeongsaeng/domain/job/dto/response/FormFieldResponseDTO.java
+++ src/main/java/com/umc/pyeongsaeng/domain/job/dto/response/FormFieldResponseDTO.java
@@ -3,6 +3,8 @@ package com.umc.pyeongsaeng.domain.job.dto.response;
 import com.umc.pyeongsaeng.domain.job.enums.FieldType;
 import lombok.*;
 
+import java.util.List;
+
 public class FormFieldResponseDTO {
 
 	@Getter
@@ -13,5 +15,15 @@ public class FormFieldResponseDTO {
 		Long id;
 		String fieldName;
 		FieldType fieldType;
+		// 질문별 선택지를 여기에 넣으면 좋을 것 같아요
+	}
+
+	@Getter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class FormFieldPreViewListDTO {
+		List<FormFieldResponseDTO.FormFieldPreViewDTO> formFieldList;
+
 	}
 }
