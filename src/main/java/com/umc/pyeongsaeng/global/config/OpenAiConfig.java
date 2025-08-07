f3f60700a368656980e0f73ebb64ebec5a1eb9c9
songhee <songhee9327@gmail.com>
2025-08-07 15:06:43 +0900

    hotfix: openAI 관련 설정 추가

--- src/main/java/com/umc/pyeongsaeng/global/config/OpenAiConfig.java
+++ src/main/java/com/umc/pyeongsaeng/global/config/OpenAiConfig.java
@@ -1,6 +1,6 @@
 package com.umc.pyeongsaeng.global.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -11,13 +11,11 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class OpenAiConfig {
 
+	@Value("${openai.api.key}")
+	private String apiKey;
+
 	@Bean(name = "openAiRestTemplate")
 	public RestTemplate openAiRestTemplate() {
-		Dotenv dotenv = Dotenv.configure()
-			.ignoreIfMissing()
-			.load();
-
-		String apiKey = dotenv.get("OPENAI_API_KEY");
 
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.getInterceptors().add((request, body, execution) -> {
