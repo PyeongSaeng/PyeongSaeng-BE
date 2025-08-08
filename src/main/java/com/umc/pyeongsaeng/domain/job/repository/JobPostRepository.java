d0f517938f4aee4363c09f5475059d49c9a6b967
parkjihyeon <wlgusqkr11@naver.com>
2025-08-08 16:17:19 +0900

    docs: 주석제거

--- src/main/java/com/umc/pyeongsaeng/domain/job/repository/JobPostRepository.java
+++ src/main/java/com/umc/pyeongsaeng/domain/job/repository/JobPostRepository.java
@@ -24,14 +24,13 @@ public interface JobPostRepository extends JpaRepository<JobPost, Long> {
            SELECT jp.*
            FROM job_post jp
            INNER JOIN (
-               -- 이 서브쿼리 안에 WHERE 절을 추가하여 7일간의 데이터만 집계합니다.
                SELECT
                    job_post_id,
                    COUNT(*) AS application_count
                FROM
                    application ap
                WHERE
-                   ap.created_at >= DATE_SUB(NOW(), INTERVAL 7 DAY) -- <<< 이 부분이 핵심 변경 사항입니다.
+                   ap.created_at >= DATE_SUB(NOW(), INTERVAL 7 DAY)
                GROUP BY
                    job_post_id
            ) AS count_subquery ON jp.id = count_subquery.job_post_id
