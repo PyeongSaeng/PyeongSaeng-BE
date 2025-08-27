7ee9c1b7b2a2e757f172186cfd1317e83ba11f8c
leewatertrue <leewatertrue@naver.com>
2025-08-27 21:52:11 +0900

    feat: redis 추가 및 k6

---
+++ k6/smoke_test.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 1,
+    duration: '30s',
+};
+
+export default function () {
+    const res = http.get('http://13.209.153.208');
+    check(res, { 'status was 200': (r) => r.status == 200 });
+    sleep(1);
+}
